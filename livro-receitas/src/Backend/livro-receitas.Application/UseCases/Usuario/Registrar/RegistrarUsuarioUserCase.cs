b7065a04412b764e5161708940ca025a3b085d04
nicaodev <alexandre.nicos@gmail.com>
2023-03-02 23:08:52 -0300

    Arquivo Resources, FluentValidator para registro de usuario

---
+++ livro-receitas/src/Backend/livro-receitas.Application/UseCases/Usuario/Registrar/RegistrarUsuarioUserCase.cs
@@ -0,0 +1,23 @@
+﻿using livro_receitas.Comunicacao.Request;
+
+namespace livro_receitas.Application.UseCases.Usuario.Registrar;
+
+public class RegistrarUsuarioUserCase
+{
+    public Task Executar(RequestRegistrarUsuarioJson request)
+    {
+        throw new Exception(); // retirar
+    }
+
+    private void Validar(RequestRegistrarUsuarioJson request)
+    {
+        var validator = new RegistrarUsuarioValidator();
+        var resultado = validator.Validate(request);
+
+        if (!resultado.IsValid)
+        {
+            var msgError = resultado.Errors.Select(e => e.ErrorMessage);
+            throw new Exception();
+        }
+    }
+}
