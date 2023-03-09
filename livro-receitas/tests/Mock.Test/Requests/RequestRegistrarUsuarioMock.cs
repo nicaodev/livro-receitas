e9f75006081755054d5fbbfdd0d0e6864ecb0bc2
nicaodev <alexandre.nicos@gmail.com>
2023-03-09 20:51:13 -0300

    Validator Tests

---
+++ livro-receitas/tests/Mock.Test/Requests/RequestRegistrarUsuarioMock.cs
@@ -0,0 +1,17 @@
+﻿using Bogus;
+using livro_receitas.Comunicacao.Request;
+
+namespace Mock.Test.Requests
+{
+    public class RequestRegistrarUsuarioMock
+    {
+        public static RequestRegistrarUsuarioJson Construir()
+        {
+            return new Faker<RequestRegistrarUsuarioJson>()
+                .RuleFor(c => c.Nome, f => f.Person.FullName)
+                .RuleFor(c => c.Email, f => f.Internet.Email())
+                .RuleFor(c => c.Senha, f => f.Internet.Password())
+                .RuleFor(c => c.Telefone, f => f.Phone.PhoneNumber("## ! ####-####").Replace("!", $"{f.Random.Int(min: 1, max: 9)}"));
+        }
+    }
+}
---
+++ livro-receitas/tests/Validators.Test/Usuario/Registrar/RegistrarUsuarioValidatorTest.cs
@@ -0,0 +1,34 @@
+﻿using FluentAssertions;
+using livro_receitas.Application.UseCases.Usuario.Registrar;
+using livro_receitas.Comunicacao.Request;
+using livro_receitas.Exceptions;
+using Mock.Test.Requests;
+using Xunit;
+
+namespace Validators.Test.Usuario.Registrar;
+
+public class RegistrarUsuarioValidatorTest
+{
+    [Fact]
+    public void Validar_Sucesso()
+    {
+        var validator = new RegistrarUsuarioValidator();
+
+        var requisicao = RequestRegistrarUsuarioMock.Construir();
+        var resultado = validator.Validate(requisicao);
+        resultado.IsValid.Should().BeTrue();
+    }
+
+    [Fact]
+    public void Validar_Erro_Nome_Vazio()
+    {
+        var validator = new RegistrarUsuarioValidator();
+
+        var requisicao = RequestRegistrarUsuarioMock.Construir();
+        requisicao.Nome = "";
+        var resultado = validator.Validate(requisicao);
+
+        resultado.IsValid.Should().BeFalse();
+        resultado.Errors.Should().ContainSingle().And.Contain(e => e.ErrorMessage.Equals("The user's name is required."/*ResourceMensagensDeErro.NOME_USUARIO_EM_BRANCO*/));
+    }
+}
