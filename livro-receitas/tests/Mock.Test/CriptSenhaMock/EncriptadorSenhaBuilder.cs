b63df454206a6439bad34cead9a29da4e65f6f77
nicaodev <alexandre.nicos@gmail.com>
2023-03-11 19:53:38 -0300

    teste unidade useCase registrar_usuario

---
+++ livro-receitas/tests/Mock.Test/CriptSenhaMock/EncriptadorSenhaBuilder.cs
@@ -0,0 +1,12 @@
+﻿using livro_receitas.Application.Services.Criptografia;
+
+namespace Mock.Test.CriptSenhaMock;
+
+public class EncriptadorSenhaBuilder
+{
+
+    public static EncriptadorSenha Instancia()
+    {
+        return new EncriptadorSenha("ABCDEFGHIJKLM-NicolasAlexandre-Developer.");
+    }
+}
---
+++ livro-receitas/tests/Mock.Test/Token/TokenControllerBuilder.cs
@@ -0,0 +1,27 @@
+﻿using livro_receitas.Application.Services.Token;
+using System.Text;
+
+namespace Mock.Test.Token;
+
+public class TokenControllerBuilder
+{
+    public static TokenController Instancia()
+    {
+        return new TokenController(10, EncodeToBase64("%jCL08uq9AYcM!6WBP%7fmnQ25a9n8cHA"));
+    }
+
+    static public string EncodeToBase64(string texto)
+    {
+        try
+        {
+            byte[] textoAsBytes = Encoding.ASCII.GetBytes(texto);
+            string resultado = System.Convert.ToBase64String(textoAsBytes);
+            return resultado;
+        }
+        catch (Exception)
+        {
+            throw;
+        }
+    }
+
+}
