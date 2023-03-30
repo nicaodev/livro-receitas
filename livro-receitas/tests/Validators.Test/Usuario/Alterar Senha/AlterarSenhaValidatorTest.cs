7e7065bc079af2a77be4f20907681e22dd033be2
nicaodev <alexandre.nicos@gmail.com>
2023-03-29 22:43:37 -0300

    Alterar senha validator test

---
+++ livro-receitas/tests/Validators.Test/Usuario/Alterar Senha/AlterarSenhaValidatorTest.cs
@@ -0,0 +1,21 @@
+﻿using FluentAssertions;
+using livro_receitas.Application.UseCases.Usuario.AlterarSenha;
+using livro_receitas.Application.UseCases.Usuario.Registrar;
+using livro_receitas.Comunicacao.Request;
+using Mock.Test.Requests;
+using Xunit;
+
+namespace Validators.Test.Usuario.Registrar;
+
+public class AlterarSenhaValidatorTest
+{
+    [Fact]
+    public void Validar_Sucesso()
+    {
+        var validator = new AlterarSenhaValidator();
+
+        var requisicao = RequestAlterarSenhaUsuarioBuilder.Construir();
+        var resultado = validator.Validate(requisicao);
+        resultado.IsValid.Should().BeTrue();
+    }
+}
